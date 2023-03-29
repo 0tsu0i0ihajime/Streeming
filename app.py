e912754721b6912ddd598dc48019567571f96011
0tsu0i0ihajime <110276910+0tsu0i0ihajime@users.noreply.github.com>
2023-03-29 22:22:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
+from flask import Flask, request, send_file
+import youtube_dl
 import os
-import subprocess
-from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/play', methods=['POST'])
-def play():
-    links = request.json.get('links')
-    for link in links:
-        download(link)
-    subprocess.Popen(['mplayer', '-playlist', 'playlist.txt'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    return 'Playing'
-
-def download(link):
-    cmd = f'yt-dlp -x --audio-format mp3 -o "downloads/%(title)s.%(ext)s" {link}'
-    subprocess.run(cmd, shell=True)
-    with open('playlist.txt', 'a') as f:
-        f.write(f"downloads/{link.split('=')[1]}.mp3\n")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        link = request.form['link']
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': 'music/%(title)s.%(ext)s',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=True)
+            filename = ydl.prepare_filename(info_dict)
+        os.system(f"vlc --play-and-exit --qt-start-minimized {filename}")
+        os.remove(filename)
+        return "音楽が再生されました。"
+    return '''
+        <form method="post">
+            <label for="link">YouTubeリンク:</label>
+            <input type="text" name="link" id="link" required>
+            <input type="submit" value="再生">
+        </form>
+    '''
 
 if __name__ == '__main__':
-    if not os.path.exists('downloads'):
-        os.makedirs('downloads')
     app.run()
     
